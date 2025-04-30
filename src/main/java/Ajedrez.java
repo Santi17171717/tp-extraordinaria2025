6cee827ae37dc43875238373947b4e31358c0f02
Santiago Guinea <santiagoguinearl@gmail.com>
2025-04-30 16:21:12 +0200

    Dos

--- src/main/java/Ajedrez.java
+++ src/main/java/Ajedrez.java
@@ -1,16 +1,50 @@
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.PrintStream;
+import java.io.*;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Ajedrez {
 
     public static void main(String[] args) {
-
+        if (args.length == 3){
+            try {
+                Tablero tablero = new Tablero(args[0]);
+                if (args[1].contentEquals("negro") || args[1].contentEquals("blanco")){
+                    Color turno;
+                    if (args[1].contentEquals("negro")){
+                        turno = Color.NEGRO;
+                    }else {
+                        turno = Color.BLANCO;
+                    }
+                    jugar(tablero, turno, System.out, new Scanner(System.in), args[2]);
+                }else {
+                    System.out.println(" El argumento TURNO debe ser BLANCO o NEGRO");
+                }
+            }catch (IllegalArgumentException exception){
+                System.out.println("Error al crear el tablero");
+            }
+        } else if (args.length == 1) {
+            reproducir(args[0], System.out, new Scanner(System.in));
+        }else {
+            System.out.println("Número incorrecto de argumentos");
+        }
     }
 
     static void jugar(Tablero tablero, Color turno, PrintStream pantalla, Scanner teclado, String registro) {
+        try (FileWriter fileWriter = new FileWriter(registro)) {
+        } catch (IOException e) {
+            pantalla.println("Error al abrir el fichero de registro");
+            throw new RuntimeException();
+        }
+        do {
+            pantalla.println(tablero.toString());
 
+
+            if (turno.equals(Color.NEGRO)){
+                turno = Color.BLANCO;
+            }else {
+                turno = Color.NEGRO;
+            }
+        } while (!tablero.reyMuerto(turno));
     }
 
     static void reproducir(String fichero_movimientos, PrintStream pantalla, Scanner teclado) {
--- src/main/java/Pieza.java
+++ src/main/java/Pieza.java
@@ -1,24 +1,102 @@
 public class Pieza {
+    private TipoPieza tipoPieza;
+    private Color color;
+    private boolean movida;
 
     public Pieza(char c) {
+        this.movida = false;
+        switch (c){
+            case '♙':{
+                this.tipoPieza = TipoPieza.PEON;
+                this.color = Color.BLANCO;
+                break;
+            }
 
+            case '♟':{
+                this.tipoPieza = TipoPieza.PEON;
+                this.color = Color.NEGRO;
+                break;
+            }
+
+            case '♗':{
+                this.tipoPieza = TipoPieza.ALFIL;
+                this.color = Color.BLANCO;
+                break;
+            }
+
+            case '♝':{
+                this.tipoPieza = TipoPieza.ALFIL;
+                this.color = Color.NEGRO;
+                break;
+            }
+
+            case '♖':{
+                this.tipoPieza = TipoPieza.TORRE;
+                this.color = Color.BLANCO;
+                break;
+            }
+
+            case '♜':{
+                this.tipoPieza = TipoPieza.TORRE;
+                this.color = Color.NEGRO;
+                break;
+            }
+
+            case '♘':{
+                this.tipoPieza = TipoPieza.CABALLO;
+                this.color = Color.BLANCO;
+                break;
+            }
+
+            case '♞':{
+                this.tipoPieza = TipoPieza.CABALLO;
+                this.color = Color.NEGRO;
+                break;
+
+            }
+
+            case '♕':{
+                this.tipoPieza = TipoPieza.REINA;
+                this.color = Color.BLANCO;
+                break;
+            }
+
+            case '♛':{
+                this.tipoPieza = TipoPieza.REINA;
+                this.color = Color.NEGRO;
+                break;
+            }
+
+            case '♔':{
+                this.tipoPieza = TipoPieza.REY;
+                this.color = Color.BLANCO;
+                break;
+            }
+
+            case '♚':{
+                this.tipoPieza = TipoPieza.REY;
+                this.color = Color.NEGRO;
+                break;
+            }
+
+        }
     }
 
 
     public TipoPieza getTipoPieza() {
-
+        return tipoPieza;
     }
 
     public Color getColor() {
-
+        return color;
     }
 
     public boolean getMovida() {
-
+        return movida;
     }
 
     public void marcarMovida() {
-
+        this.movida = true;
     }
 
     @Override
