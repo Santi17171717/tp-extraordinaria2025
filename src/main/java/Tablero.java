2a38a3e95e103debced0222e855d6bc49c851faf
Santiago Guinea <santiagoguinearl@gmail.com>
2025-05-01 13:17:29 +0200

    Seis

--- src/main/java/Tablero.java
+++ src/main/java/Tablero.java
@@ -39,15 +39,70 @@ public class Tablero {
     }
 
     boolean comprobarPiezasEnCamino(Movimiento movimiento) {
+        boolean caminoLibre = true;
+        int i, j;
 
+        switch (movimiento.toString().charAt(0)){
+            case 'P':{
+
+            }case 'A':{
+
+            }case 'C':{
+                return true;
+            }case 'T':{
+
+            }case 'R':{
+                if (movimiento.toString().charAt(1) == movimiento.toString().charAt(3)){ //vertical
+
+                }else if (movimiento.toString().charAt(2) == movimiento.toString().charAt(4)){ // horizontal
+
+                }else {//diagonal
+
+                    do {
+                        if (tablero[i][j] != null){
+                            caminoLibre = false;
+                        }
+                        if (){ // arriba izq
+
+                        } else if () { // arriba der
+
+                        } else if () { // abajo izq
+
+                        }else { // abajo der
+
+                        }
+                    }while ((movimiento.toString().charAt(2) != (movimiento.toString().charAt(4));
+                }
+            }case 'K':{
+
+            }
+        }
+        return caminoLibre;
     }
 
     public void agregarPieza(int fila, int columna, Pieza pieza) {
-
+        if (fila < 8 && columna < 8 && tablero[fila][columna] == null){
+            tablero[fila][columna] = pieza.toString();
+        }
+        else if (fila >= 8 || columna >= 8){
+            throw new IllegalArgumentException("La casilla (" + fila + ", "+ columna + ") está fuera de rango con " +
+                    "las coordenadas correspondientes");
+        }else {
+            throw new IllegalArgumentException("La casilla (" + fila + ", "+ columna + ") está ocupada por la pieza "
+                    + pieza);
+        }
     }
 
     public Pieza getPieza(int fila, int columna) {
+        if (fila < 8 && columna < 8){
+            String posicion = tablero[fila][columna];
 
+            return
+        }
+        else {
+            throw new IllegalArgumentException("La casilla (" + fila + ", "+ columna + ") está fuera de rango con " +
+                    "las coordenadas correspondientes");
+        }
     }
 
     public String toString() {
